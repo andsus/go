0901de69706d72916f83c0200c85e6077e720255
Andre Susantin <andre.susantin@google.com>
2021-08-29 16:58:50 -0700

    aug2021

--- clock/clock.go
+++ clock/clock.go
@@ -1,80 +1,35 @@
 package clock
 
-// import (
-// 	"fmt"
-// )
+import (
+	"fmt"
+)
 
-// // Clock is type
-// type Clock struct {
-// 	m int
-// }
-
-// // New function to create Clock
-// func New(hour, minute int) (c Clock) {
-// 	m := hour*60 + minute
-// 	m %= 24 * 60
-// 	if m < 0 {
-// 		m += 24 * 60
-// 	}
-// 	return Clock{m}
-// }
-
-// // Add function add to Clock, can be negative
-// func (c Clock) Add(minutes int) Clock {
-// 	return New(0, c.m+minutes)
-// }
-
-// // Subtract function add to Clock, can be negative
-// func (c Clock) Subtract(minutes int) Clock {
-// 	return c.Add(-minutes)
-// }
-
-// // String prints the time a Clock represents
-// func (c Clock) String() string {
-// 	return fmt.Sprintf("%02d:%02d", c.m/60, c.m%60)
-// }
-
-import "fmt"
-
-const minutesInAnHour = 60
-const hoursInADay = 24
-const minutesInADay = minutesInAnHour * hoursInADay
-
-// Clock that handles time (hours and minutes) without dates
+// Clock is type
 type Clock struct {
-	minutes int
+	m int
 }
 
-// New creates a new clock
-func New(hours int, minutes int) Clock {
-	minutes = hours*minutesInAnHour + minutes
-	wrappedMinutes := (minutes%minutesInADay + minutesInADay) % minutesInADay
-	return Clock{wrappedMinutes}
+// New function to create Clock
+func New(hour, minute int) (c Clock) {
+	m := hour*60 + minute
+	m %= 24 * 60
+	if m < 0 {
+		m += 24 * 60
+	}
+	return Clock{m}
 }
 
-// func new(minutes int) Clock {
-// 	wrappedMinutes := (minutes%minutesInADay + minutesInADay) % minutesInADay
-// 	return Clock{wrappedMinutes}
-// }
-
-func (c Clock) String() string {
-	hours, minutes := divide(c.minutes, minutesInAnHour)
-	return fmt.Sprintf("%02d:%02d", hours, minutes)
-}
-
-type quotient = int
-type remainder = int
-
-func divide(dividend int, divisor int) (quotient, remainder) {
-	return dividend / divisor, dividend % divisor
-}
-
-// Add a given amount of minutes to this Clock
+// Add function add to Clock, can be negative
 func (c Clock) Add(minutes int) Clock {
-	return New(0, c.minutes+minutes)
+	return New(0, c.m+minutes)
 }
 
-// Subtract a given amount of minutes from this Clock
+// Subtract function add to Clock, can be negative
 func (c Clock) Subtract(minutes int) Clock {
-	return New(0, c.minutes-minutes)
+	return c.Add(-minutes)
+}
+
+// String prints the time a Clock represents
+func (c Clock) String() string {
+	return fmt.Sprintf("%02d:%02d", c.m/60, c.m%60)
 }
